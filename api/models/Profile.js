cb3c8142ded85634e6c8d5baed067ffd5d916782
Anmol Shukla <anmol01shukla@gmail.com>
2018-09-09 17:53:15 +0530

    Added the user profile model.

---
+++ api/models/Profile.js
@@ -0,0 +1,31 @@
+/**
+ * Profile.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  autoPK: true,
+  attributes: {
+    userId: {
+      type: 'number',
+      required: true,
+      unique: true
+    },
+    questionsSolvedTotal: {
+      type: 'number'
+    },
+    questionsAttemptedTotal: {
+      type: 'number'
+    },
+    questionsSolvedCurrent: {
+      type: 'number'
+    },
+    questionsAttemptedCurrent: {
+      type: 'number'
+    }
+  }
+};
+
